e3a5ab84bdbd96fe44f31b82d95248d2ee963b70
hiroto-ito4 <ap.otorih6230@icloud.com>
2021-02-24 12:46:17 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :lastname, presence: true
+  validates :firstname, presence: true
+  validates :last_furigana, presence: true
+  validates :first_furigana, presence: true
+  validates :birthday, presence: true
 end
