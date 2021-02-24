b04de53775f8305d477a5f5cf209f04e131852a4
hiroto-ito4 <ap.otorih6230@icloud.com>
2021-02-24 19:46:17 +0900

    新規登録ページにバリデーション記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email
-  validates :password
+
+  validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "should be a mixture of half-width numbers and English"}
   validates :nickname, presence: true
-  validates :lastname, presence: true
-  validates :firstname, presence: true
-  validates :last_furigana, presence: true
-  validates :first_furigana, presence: true
+  validates :lastname, presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]/, message: "Fill in full-width hiragana, katakana, and kanji"}
+  validates :firstname, presence: true, format: { with:/\A[ぁ-んァ-ン一-龥]/, message: "Fill in full-width hiragana, katakana, and kanji"}
+  validates :last_furigana, presence: true, format: { with:/\p{katakana}/, message: "Fill in full-width katakana"}
+  validates :first_furigana, presence: true, format: { with:/\p{katakana}/, message: "Fill in full-width katakana"}
   validates :birthday, presence: true
 end
