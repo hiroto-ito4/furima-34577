e604cf3f3286be934ded04fd87a538fc03bc4752
hiroto-ito4 <ap.otorih6230@icloud.com>
2021-02-26 23:49:34 +0900

    モデルの記入、バリデーションの記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
 
-  belongs_to : user
+  belongs_to :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -9,4 +9,18 @@ class Item < ApplicationRecord
   belongs_to :shipping
   belongs_to :area
   belongs_to :estimate
+
+  with_options presence: true do 
+    validates :product_name
+    validates :description
+    validates :price
+    validates :image
+  end
+  with_options numericality: { other_than: 1 }
+    validates :category_id
+    validates :status_id
+    validates :shipping_id
+    validates :area_id
+    validates :estimate_id
+  end
 end
