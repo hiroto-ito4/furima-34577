d0208efcbf2ed645745b10fa4830eb83eda906d5
hiroto-ito4 <ap.otorih6230@icloud.com>
2021-03-04 19:20:00 +0900

    formオブジェクトの導入　その他修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,24 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: :index
 
   def index
-  end
-
-  def new
+    @purchase_information = PurchaseInformation.new
   end
 
   def create
+    @purchase_information = PurchaseInformation.new(purchase_params)
+    if @purchase_information.valid?
+      @purchase_information.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+  private
+
+    def purchase_params
+      params.require(:purchase_information).permit( :postalcode, :area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id)
+    end
+
 end
--- app/models/information.rb
+++ app/models/information.rb
@@ -1,11 +1,3 @@
 class Information < ApplicationRecord
   belongs_to :purchase
-
-  with_options presence: true do
-    validates :postalcode, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :municipality 
-    validates :address
-    validates :phone_number, numericality: { only_integer: true ,message: 'Please enter only half-width numbers'}
-  end
-    validates :area_id, numericality: { other_than: 1, message: 'Please select' }
 end
