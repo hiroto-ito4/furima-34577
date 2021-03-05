2b769d2b89441c774ddeb6b112f399ae1d523806
hiroto-ito4 <ap.otorih6230@icloud.com>
2021-03-05 12:46:28 +0900

    エラーメッセージの順番調整

--- app/models/purchase_information.rb
+++ app/models/purchase_information.rb
@@ -2,13 +2,13 @@ class PurchaseInformation
   include ActiveModel::Model
   attr_accessor :postalcode, :area_id, :municipality, :address, :building_name, :phone_number, :purchase_id, :item_id, :user_id
 
+  validates :postalcode, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :area_id, numericality: { other_than: 1, message: 'Please select' }
   with_options presence: true do
-    validates :postalcode, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality 
     validates :address
     validates :phone_number, numericality: { only_integer: true ,message: 'Please enter only half-width numbers'}
   end
-  validates :area_id, numericality: { other_than: 1, message: 'Please select' }
 
     def save
       purchase = Purchase.create(item_id: item_id, user_id: user_id)
